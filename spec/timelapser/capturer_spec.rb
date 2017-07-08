9de7a181f520b6165ab3a83d67ff6a95bfdc04c5
Alex Clink <code@alexclink.com>
2017-07-08 14:43:19 -0400

    Add expectation for file size

--- spec/timelapser/capturer_spec.rb
+++ spec/timelapser/capturer_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe Capturer do
     loop until not c.running?
     expect(c.capture_count).to eq(10)
     expect(File.exists?("#{c.save_path}/#{c.folder}.mp4")).to be_truthy
+    expect(File.size("#{c.save_path}/#{c.folder}.mp4")).not_to eq(0)
     system "rm -rf #{c.save_path}"
   end
 end
